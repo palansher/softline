b3625b77495f85e5cf2d695d0d07d9efc90a39f2
Vladimir Perepechenko <palansher@outlook.com>
2026-07-06 02:40:45 +0300

    add docker postgres image

---
+++ m3_1h/.infra/postgres/init-scripts/init.sql
@@ -0,0 +1,14 @@
+-- 1. Создаем/обновляем пользователя
+CREATE USER shop_admin WITH PASSWORD 'admin';
+ALTER USER shop_admin WITH PASSWORD 'admin';
+
+-- 2. Создаем базу (без блока DO)
+-- Если она уже есть при случайном ручном запуске, Postgres просто выдаст варнинг/ошибку в лог, но пойдет дальше
+CREATE DATABASE shop_db_hw3_1;
+
+-- 3. Навешиваем права на саму базу
+GRANT ALL PRIVILEGES ON DATABASE shop_db_hw3_1 TO shop_admin;
+
+-- 4. Переключаемся на созданную базу и даем права на работу со схемами (очень важно для Postgres 15+)
+\c shop_db_hw3_1
+GRANT ALL ON SCHEMA public TO shop_admin;
--- m3_1h/Readme.md
+++ m3_1h/Readme.md
@@ -2,3 +2,13 @@
 
 Разбор ДЗ в видео 3.4 <https://my.mts-link.ru/j/AcademySL/pp_python-devsecops/record-new/19410065662>
 на 05:54
+
+```bash
+cd /home/vp/code/learn-python/m3_1h`
+cd /home/vp/code/learn-python/m3_1h/.infra/postgres
+
+docker compose up -d
+
+#  удаляет именованные тома
+docker compose down -v
+```
