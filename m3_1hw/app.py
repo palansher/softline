7c9f6c756f85b268ff9215e058ff9e08be345622
Vladimir Perepechenko <palansher@outlook.com>
2026-07-07 01:29:54 +0300

    удалил мусор, добавил шаблон карточки товара

--- m3_1hw/app.py
+++ m3_1hw/app.py
@@ -14,9 +14,9 @@ def contacts() -> str:
     return render_template("contacts.html")
 
 
-
 # def catalog() -> str:
 #     return render_template("catalog.html")
+
 
 @app.route("/catalog")
 def catalog():
@@ -26,12 +26,28 @@ def catalog():
     data = cursor.fetchall()
     return render_template("catalog.html", items=data)
 
-@app.template_filter('format_price')
+
+@app.route("/catalog/<int:car_id>")
+def car_detail(car_id: int):
+    # Запрашиваем из VIEW конкретную машину по id
+    sql = "SELECT * FROM v_catalog_display WHERE car_id = %s;"
+    cursor.execute(sql, (car_id,))
+    car = cursor.fetchone()
+
+    # Если машина не найдена (например, ввели неверный ID в URL), отдаем ошибку 404
+    if not car:
+        return "Автомобиль не найден", 404
+
+    return render_template("car-detail.html", car=car)
+
+
+@app.template_filter("format_price")
 def format_price(value):
     if value is None:
         return "0 ₽"
     # Форматируем число с разделением тысяч пробелами, без копеек
     return f"{int(value):,}".replace(",", " ") + " ₽"
+
 
 # <p class="text-muted">{{ car.full_info | linebreaksbr }}</p>
 # фильтр linebreaksbr автоматически заменит обычные переносы строк на теги <br>
