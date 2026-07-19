ea783f9de7fdcccffdf5decf4b5ecb242d403ccd
Vladimir Perepechenko <palansher@outlook.com>
2026-07-19 05:54:35 +0300

    перенес регистрацию маршрутов в __init__.py

--- m3_2hw/app.py
+++ m3_2hw/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from routes.main import main_bp
-from routes.auth import auth_bp
-from routes.catalog import catalog_bp
-from routes.cart import cart_bp
-from routes.admin import admin_bp
+# from routes.main import main_bp
+# from routes.auth import auth_bp
+# from routes.catalog import catalog_bp
+# from routes.cart import cart_bp
+# from routes.admin import admin_bp
+
+from routes import main_bp, auth_bp, catalog_bp, cart_bp, admin_bp
 
 app = Flask(__name__)
 app.secret_key = "drivex_super_secret_session_key_2026"
--- m3_2hw/routes/__init__.py
+++ m3_2hw/routes/__init__.py
@@ -1,1 +1,18 @@
 # Routes package initialization
+
+from .main import main_bp
+from .auth import auth_bp
+from .catalog import catalog_bp
+from .cart import cart_bp
+from .admin import admin_bp
+
+# определяем специальный список __all__, в котором перечисляем  строки с именами объектов, доступных для экспорта из этого пакета.
+# Это типа явный стандарт в Python-разработке.
+
+__all__ = [
+    "main_bp",
+    "auth_bp",
+    "catalog_bp",
+    "cart_bp",
+    "admin_bp",
+]
