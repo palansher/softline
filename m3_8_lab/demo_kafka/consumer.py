83798caf24880138bb2738d7ebbe8e68668cf671
Vladimir Perepechenko <palansher@outlook.com>
2026-07-31 19:32:40 +0300

    Завершил модуль 3.8

--- m3_8_lab/demo_kafka/consumer.py
+++ m3_8_lab/demo_kafka/consumer.py
@@ -1,7 +1,6 @@
-import configparser
 import logging
 
-from confluent_kafka import Consumer, KafkaError, KafkaException
+from confluent_kafka import Consumer, KafkaException
 
 logging.basicConfig(
     level=logging.INFO, format="{asctime} - {name} {levelname} {message}", style="{"
@@ -18,7 +17,7 @@ def main():
         "auto.offset.reset": "earliest",  # Стратегия при отстутсвии сохраненного offset:
         # 1) earliest: читаем с самого начала топика
         # 2) latest: читаем только новые соообщения. Забываем, что было раньше.
-        # 2) none: завершаем работу ошибкой
+        # 3) none: завершаем работу ошибкой
         "enable.auto.commit": True,  # включение автоматического подтверждения обработки сообщения. Обязательно для консюмеров.
     }
 
@@ -28,33 +27,51 @@ def main():
     try:
         # Подписываемся на топик или группу топиков
         consumer.subscribe([topic])
-        logger.info(f"Успешная подписка на топик {topic}")
+        logger.info("Успешная подписка на топик %s", topic)
 
         # Бесконечный цикл для обработки сообщений
         while True:
             msg = consumer.poll(1.0)  # проверяем наличие новых сообщений каждую секунду
             if msg is None:
-                continue # Если сообщений нет, то пропускаем.
+                continue  # Если сообщений нет, то пропускаем.
             if msg.error():
-                logger.error(f"Ошибка при получении сообщения (poll): {msg.error()}")
-                continue # Пропускаем, если нет ошибок
+                logger.error("Ошибка при получении сообщения (poll): %s", msg.error())
+                continue  # Пропускаем, если нет ошибок
             try:
-                key = msg.key().decode("utf8") if msg.key() else None
-                value = msg.value().decode("utf8") if msg.value() else None
+                
+                """
+                "decode" is not a known attribute of "None" Pylance (reportOptionalMemberAccess)
+                
+                Эта ошибка возникает из-за особенностей работы анализатора типов (Type Checker) в Pylance.
+
+                Когда мы пишем msg.key().decode(...) if msg.key() is not None else None, Pylance делает два отдельных вызова метода msg.key().
+                Тайпчекер не может гарантировать, что вызов метода дважды подряд вернёт одно и то же значение,
+                поэтому он не делает сужение типов (Type Narrowing) для первого вызова и продолжает считать, что msg.key() может вернуть None.
+                
+                Решение
+                Нужно сохранить результат msg.key() и msg.value() в промежуточные переменные.
+                Тогда Pylance поймет, что после проверки if raw_key is not None переменная гарантированно имеет тип bytes:
+                """                
+                raw_key = msg.key()
+                raw_value = msg.value()
+
+                key = raw_key.decode("utf-8") if raw_key is not None else None
+                value = raw_value.decode("utf-8") if raw_value is not None else None
+
                 logger.info(
-                    f"Получено: topic: %s, partition: %s, offset: %s, key:%s, value: %s",
+                    "Получено: topic: %s, partition: %s, offset: %s, key:%s, value: %s",
                     topic,
                     msg.partition(),
                     msg.offset(),
                     key,
                     value,
                 )
-            except (KafkaException, KafkaError) as e:
-                logger.error(f" Ошибка при получении ключа и значения из сообщения: {e}")
-    except Exception as e:
-        logger.error("Критическая ошибка", e)
+            except (KafkaException, UnicodeDecodeError) as e:
+                logger.error(" Ошибка при получении ключа и значения из сообщения: %s", e)
     except KeyboardInterrupt:
         logger.error("Прервано пользователем")
+    except Exception as e:  # noqa: BLE001
+        logger.error("Критическая ошибка: %s", e)
     finally:
         consumer.close()
         logger.info("Консьюмер закрыт!")
